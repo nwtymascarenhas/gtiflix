543d28db9aee931f425b47a6b1a630e4f0870784
nwtymascarenhas <nwtymascarenhas@gmail.com>
2025-09-18 10:48:28 -0300

    instalacao tailwind e teste de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/naty', function () { 
+    echo 'ola naty';
+ });
+
+ route::get('/filmes/{filme}', function($filme) { 
+    echo 'Acessando dados do filme: ' . $filme;
+ });
+  
+ Route::view('/conheca ' , 'sobre');
+
+ 
